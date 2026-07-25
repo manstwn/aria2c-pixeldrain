67e0574e953f0fe639e83a196c006a14547c4f31
Imans <imansetiawan1998@gmail.com>
2026-07-25 07:22:07 +0700

    fix: import missing fs module in server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const path = require('path');
+const fs = require('fs');
 require('dotenv').config();
 
 const db = require('./src/db');
